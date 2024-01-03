417f879a37cac37976abef3cfda782781dda86da
Yasin YILDIZ <144591826+yasinfedev@users.noreply.github.com>
2024-01-03 12:50:36 +0300

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -7,35 +7,35 @@ const Login = () => {
     <>
       <Navbar />
       <div className="container my-3 py-3">
-        <h1 className="text-center">Login</h1>
+        <h1 className="text-center">Giriş</h1>
         <hr />
         <div class="row my-4 h-100">
           <div className="col-md-4 col-lg-4 col-sm-8 mx-auto">
             <form>
               <div class="my-3">
-                <label for="display-4">Email address</label>
+                <label for="display-4">Eposta adresiniz</label>
                 <input
                   type="email"
                   class="form-control"
                   id="floatingInput"
-                  placeholder="name@example.com"
+                  placeholder="isim@ornek.com"
                 />
               </div>
               <div class="my-3">
-                <label for="floatingPassword display-4">Password</label>
+                <label for="floatingPassword display-4">Şifreniz</label>
                 <input
                   type="password"
                   class="form-control"
                   id="floatingPassword"
-                  placeholder="Password"
+                  placeholder="Şifreniz"
                 />
               </div>
               <div className="my-3">
-                <p>New Here? <Link to="/register" className="text-decoration-underline text-info">Register</Link> </p>
+                <p>Yeni misiniz? <Link to="/register" className="text-decoration-underline text-info">Kayıt ol</Link> </p>
               </div>
               <div className="text-center">
                 <button class="my-2 mx-auto btn btn-dark" type="submit" disabled>
-                  Login
+                  Giriş yap
                 </button>
               </div>
             </form>
