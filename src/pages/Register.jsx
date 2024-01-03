e65bf1d62487d3c466e9564418e5490f5e42ffdc
Yasin YILDIZ <144591826+yasinfedev@users.noreply.github.com>
2024-01-03 12:53:49 +0300

    Update Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -6,44 +6,44 @@ const Register = () => {
         <>
             <Navbar />
             <div className="container my-3 py-3">
-                <h1 className="text-center">Register</h1>
+                <h1 className="text-center">Kayıt ol</h1>
                 <hr />
                 <div class="row my-4 h-100">
                     <div className="col-md-4 col-lg-4 col-sm-8 mx-auto">
                         <form>
                             <div class="form my-3">
-                                <label for="Name">Full Name</label>
+                                <label for="Name">Adınız Soyadınız</label>
                                 <input
                                     type="email"
                                     class="form-control"
                                     id="Name"
-                                    placeholder="Enter Your Name"
+                                    placeholder="Adınızı giriniz"
                                 />
                             </div>
                             <div class="form my-3">
-                                <label for="Email">Email address</label>
+                                <label for="Email">Eposta adresiniz</label>
                                 <input
                                     type="email"
                                     class="form-control"
                                     id="Email"
-                                    placeholder="name@example.com"
+                                    placeholder="isim@ornek.com"
                                 />
                             </div>
                             <div class="form  my-3">
-                                <label for="Password">Password</label>
+                                <label for="Password">Şifreniz</label>
                                 <input
                                     type="password"
                                     class="form-control"
                                     id="Password"
-                                    placeholder="Password"
+                                    placeholder="Şifreniz"
                                 />
                             </div>
                             <div className="my-3">
-                                <p>Already has an account? <Link to="/login" className="text-decoration-underline text-info">Login</Link> </p>
+                                <p>Zaten hesabınız var mı? <Link to="/login" className="text-decoration-underline text-info">Giriş yap</Link> </p>
                             </div>
                             <div className="text-center">
                                 <button class="my-2 mx-auto btn btn-dark" type="submit" disabled>
-                                    Register
+                                    Kayıt ol
                                 </button>
                             </div>
                         </form>
